4220f8ad9321b8734f63eb275817f3ed738a43e2
“zannew“ <wonny21292@gmail.com>
2021-05-17 22:31:10 +0900

    commit test

--- day03/template/plot.py
+++ day03/template/plot.py
@@ -31,6 +31,6 @@ class Plot:
         f, ax = plt.subplots(1, 2, figsize=(18, 8))
         this['Survived'][this['Sex'] == 'male'].value_counts().plot.pie(explode=[0, 0.1], autopct='%1.1f%%', ax=ax[0], shadow=True)
         this['Survived'][this['Sex'] == 'female'].value_counts().plot.pie(explode=[0, 0.1], autopct='%1.1f%%', ax=ax[1], shadow=True)
-        ax[0].set_title('남성의 생존비율 [0. 사망자 vs 1.생존자]')
-        ax[1].set_title('여성의 생존비율 [0. 사망자 vs 1.생존자]')
+        ax[0].set_title('남성의 생존비율 [0.사망자 vs 1.생존자]')
+        ax[1].set_title('여성의 생존비율 [0.사망자 vs 1.생존자]')
         plt.show()
